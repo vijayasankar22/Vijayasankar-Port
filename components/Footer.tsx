4adefe0cea20aec88ae5a089647cf484b915f195
Vijayasankar P <108816020+vijayasankar22@users.noreply.github.com>
2025-11-06 12:09:24 +0530

    Fix typo in footer copyright text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="py-8 border-t border-[#d6044a]/20">
       <div className="container mx-auto px-6 md:px-12 text-center text-gray-500">
-        <p>&copy; {new Date().getFullYear()} Vijayasankar P.</p>
+        <p>&copy; {new Date().getFullYear()} Vijayasankar P</p>
         
       </div>
     </footer>
